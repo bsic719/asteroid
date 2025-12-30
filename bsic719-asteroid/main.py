b09b69eecbf693e28b96c8f51f5881377abc811f
bsic719 <besic719@gmail.com>
2025-12-29 20:57:36 -0500

    updated main.py, created logger.py, and constants.py

--- bsic719-asteroid/main.py
+++ bsic719-asteroid/main.py
@@ -1,7 +1,23 @@
 import pygame
+from logger import log_state
+from constants import *
 
 def main():
     print(f"Starting Asteroids with pygame vesion: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+        screen.fill('black')
+        pygame.display.flip()
+        # refresh the screen/MAKE SURE TO CALL LAST
 
 
 if __name__ == "__main__":
